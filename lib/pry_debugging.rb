17e22a4ad5f869d6e8c20db6537173f2ab40262b
kip_parcell <kipluis@gmail.com>
2020-10-20 21:30:16 -0700

    Done.

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,4 +1,11 @@
+# I can get it to pass in pry by assigning a value of num = 3
+# then saying num + 2 gets 5 
+# how can the method do this 
+require 'pry'
+require './lib/pry_debugging.rb'
+
+
 def plus_two(num)
-	num + 2
-	num
+	num = 3 + 2
+	binding.pry
 end
